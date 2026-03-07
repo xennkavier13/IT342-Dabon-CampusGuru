c39d5a1e6d55847dd94e4d6163e124098e20fad6
LAB13_USER ON WS41 <L13Y09W41@LabHCI.net>
2026-03-07 08:59:39 +0800

    setup: gitginore and backend initial setup

---
+++ backend/src/main/java/edu/cit/dabon/campusguru/CampusguruApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.dabon.campusguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusguruApplication.class, args);
+	}
+
+}
