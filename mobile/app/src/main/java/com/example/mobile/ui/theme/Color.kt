1ed4e9d15869d92932571c0afe6df1b50aeb0f2b
xennkavier13 <kennxavier15@gmail.com>
2026-03-28 19:26:22 +0800

    feat: refined ui for mobile

--- mobile/app/src/main/java/com/example/mobile/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/example/mobile/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BrandPrimary = Color(0xFF155DFC)
+val BrandPrimaryDark = Color(0xFF0E47C4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TextPrimary = Color(0xFF000000)
+val TextSecondary = Color(0xFF5F6368)
+val SurfaceBackground = Color(0xFFF8FAF9)
